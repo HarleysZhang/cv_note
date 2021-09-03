82888de7c130c447719b48f155e32ea8754c36a5
HarleysZhang <zhg5200211@outlook.com>
2021-09-04 00:58:27 +0800

    Add coco label process code of YOLOv3

--- 6-computer_vision/目标检测/simple_yolov2/encoder.py
+++ 6-computer_vision/目标检测/simple_yolov2/encoder.py
@@ -6,7 +6,6 @@ import torch
 
 from utils import box_iou, box_nms, meshgrid, softmax
 
-
 class DataEncoder:
     def __init__(self):
         self.anchors = [(1.3221,1.73145),(3.19275,4.00944),(5.05587,8.09892),(9.47112,4.84053),(11.2364,10.0071)]

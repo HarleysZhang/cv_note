2fe2204d1af29f6311474587391ec83f6e14f78e
真锋 <zhg@MacBook-Pro.local>
2024-08-15 20:13:11 +0800

    refactor: remove data_structure and algorithm directory

--- 3-data_structure-algorithm/linked_list/52-GetIntersectionNode.cpp
+++
@@ -1,2 +0,0 @@
-// 剑指 offer 52: 两个链表的第一个公共节点 https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
-// 输入两个链表，找出它们的第一个公共节点。
--- 3-data_structure-algorithm/tree/55.2-IsBalanced.cpp
+++
@@ -1,3 +0,0 @@
-// 剑指 offer 55.2: 平衡二叉树 https://leetcode-cn.com/problems/balanced-binary-tree/
-// 给定一个二叉树，判断它是否是高度平衡的二叉树。
-
